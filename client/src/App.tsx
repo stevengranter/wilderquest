cd0f589b4802d4e252a0f15ae42e1f6abd2c3c08
Steven Granter <stevengranter@gmail.com>
2025-03-07 13:50:41 -0330

    Add dependencies for BlockNote for inline text editing to package.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
-import { Button } from "./components/ui/button";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/shadcn";
+import "@blocknote/shadcn/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
 
 export default function App() {
-  return <Button>Hello</Button>;
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote({});
+
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} />;
 }
