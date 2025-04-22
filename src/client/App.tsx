34ba4739ded2639212af8d6297ab6799cfb4e6d6
Steven Granter <stevengranter@gmail.com>
2025-04-22 14:37:02 -0230

    Move ThreeFiberCanvas to from App to Home component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,7 +3,6 @@ import {Outlet} from "react-router";
 import {Toaster} from "@/components/ui/toaster";
 import {AuthProvider} from "@/hooks/useAuth";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ThreeFiberCanvas} from "@/components/3d/ThreeFiberCanvas";
 
 
 const queryClient = new QueryClient()
@@ -12,9 +11,9 @@ export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <AuthProvider>
-                <ThreeFiberCanvas/>
                 <Outlet/>
                 <Toaster/>
+
             </AuthProvider>
         </QueryClientProvider>
     );
