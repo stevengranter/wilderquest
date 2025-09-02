ef0736e72891485aeee157ccd40f2272d37b3900
Steven Granter <stevengranter@gmail.com>
2025-09-02 15:58:29 -0230

    Removed unused resizeImage, blobToBase64 in utils.ts (removed image-blog-reduce from package.json)

--- src/client/lib/utils.ts
+++ src/client/lib/utils.ts
@@ -1,29 +1,9 @@
 // WARNING: this file is overwritten when running 'dlx shadcn@latest init'
 
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import imageBlobReduce from 'image-blob-reduce'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-export async function resizeImage(image: Blob, maxSize = 1000): Promise<Blob> {
-    const reduce = imageBlobReduce()
-    return await reduce.toBlob(image, { max: maxSize })
-}
-
-export async function blobToBase64(blob: Blob): Promise<string> {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader()
-        reader.onloadend = () => {
-            if (reader.result && typeof reader.result === 'string') {
-                resolve(reader.result)
-            } else {
-                reject('Failed to read file')
-            }
-        }
-        reader.onerror = reject
-        reader.readAsDataURL(blob)
-    })
-}
