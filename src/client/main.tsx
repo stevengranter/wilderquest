0dcaddd8484a67d9759e162a3501fa5891fcfae8
Steven Granter <stevengranter@gmail.com>
2025-09-09 14:59:35 -0230

    Remove unused css files (moved from /src/client/styles/base to parent folder)

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,8 +1,7 @@
-// src/client/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/base/globals.css' // assuming you have global styles
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
